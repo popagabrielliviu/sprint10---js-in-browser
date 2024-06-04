b1d499467a548e3df40a4181a9274387851b76c0
gabriel <liviu.gg@gmail.com>
2024-06-04 17:51:49 +0300

    tema

---
+++ script.js
@@ -0,0 +1,11 @@
+const message = "Un email a fost trimis la adresa myemail@gmail.com. Va multumesc !"
+window.alert(message)
+
+
+
+button.addEventListener('click', myFunction) 
+
+function myFunction() {
+   alert('Un email la adresa myemail@gmail.com a fost trimis. Va multumim!');
+    // document.getElementById("trigger").innerHTML = 'Un email la adresa myemail@gmail.com a fost trimis. Va multumim!';
+}
